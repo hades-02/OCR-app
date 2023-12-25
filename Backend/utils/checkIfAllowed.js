e01ebbaee7a6c0bdbb626dd736205f957d38503e
hades-02 <akshatgoyal756@gmail.com>
2023-12-25 17:34:50 +0530

    Final commit

--- Backend/utils/checkIfAllowed.js
+++ Backend/utils/checkIfAllowed.js
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
 // Check if record contains any invalid fields
-=======
->>>>>>> a35921303344189dbd5d01bcd6eca532d66b1705
 const checkIFAllowed = data => {
   const allowedFields = [
     'idNum',
@@ -15,11 +12,8 @@ const checkIFAllowed = data => {
   Object.keys(data).forEach(el => {
     if (!allowedFields.includes(el)) return false;
   });
-<<<<<<< HEAD
 
   return true;
-=======
->>>>>>> a35921303344189dbd5d01bcd6eca532d66b1705
 };
 
 module.exports = checkIFAllowed;
